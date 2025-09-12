e6fa04ddb956ecc56e1c99fcf54a9c7e5082a300
Debnitya Dutta Gupta <debnitya525@gmail.com>
2025-09-12 18:12:17 +0530

    Added Docker File

--- LeaderboardAPI/LeaderboardAPI/Program.cs
+++ LeaderboardAPI/LeaderboardAPI/Program.cs
@@ -57,7 +57,9 @@ app.MapPost("/leaderboard", async (HttpRequest request) =>
     return Results.Ok(new { success = true, message = "Score submitted" });
 });
 
-app.Run();
+//app.Run();
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Run($"http://0.0.0.0:{port}");
 
 public class PlayerScore
 {
